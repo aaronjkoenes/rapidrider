3276248ba161f770db91fa1cc52a97eb6dd1a030
veenified <veenified@1bd01c38-8c04-11dd-931e-57b92fbd641c>
2008-11-21 20:45:57 +0000

    I plan on reworking our whole project...

---
+++ branches/bmv5/RapidRider/rapidrider/SimpleLoc.java
@@ -0,0 +1,46 @@
+package rapidrider;
+
+public class SimpleLoc {
+	
+	private double myLat;
+
+	private double myLon;
+
+	public SimpleLoc() {
+		myLat = Double.NaN;
+		myLon = Double.NaN;
+	}
+
+	public SimpleLoc(double lat, double lon) {
+		myLat = lat;
+		myLon = lon;
+	}
+
+	public double getLat() {
+		return myLat;
+	}
+
+	public double getLon() {
+		return myLon;
+	}
+
+	public void setLat(double lat) {
+		myLat = lat;
+	}
+
+	public void setLon(double lon) {
+		myLon = lon;
+	}
+
+	public String printLoc() {
+		return String.valueOf(myLat) + ", " + String.valueOf(myLon);
+	}
+
+	public double distanceTo(SimpleLoc tar) {
+		double dist = Math.sqrt( 
+			 ( (Math.abs(myLat) - Math.abs(tar.getLat()) ) * ( Math.abs(myLat) - Math.abs(tar.getLat()) ) ) +
+			 ( (Math.abs(myLon) - Math.abs(tar.getLon()) ) * ( Math.abs(myLon) - Math.abs(tar.getLon()) ) )
+			 );
+		return dist;
+	}
+}
