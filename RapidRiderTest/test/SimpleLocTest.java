49fcf4775d6658e94921ff685e139f73481f2f08
anyninbign <anyninbign@1bd01c38-8c04-11dd-931e-57b92fbd641c>
2008-12-13 19:52:23 +0000

    I tested some of the easy functions.

--- RapidRiderTest/test/SimpleLocTest.java
+++ RapidRiderTest/test/SimpleLocTest.java
@@ -13,6 +13,9 @@ public class SimpleLocTest extends TestCase {
 
 	public void testDistanceTo() {
 		assertEquals(100.0, myLoc.distanceTo(new SimpleLoc(131.1, 90.2)), 0.01);
+		assertEquals(70.0, myLoc.distanceTo(new SimpleLoc(31.1, 20.2)), 0.01);
+		assertEquals(110.4, myLoc.distanceTo(new SimpleLoc(31.1, -20.2)), 0.01);
+		assertEquals(5.0, new SimpleLoc(0, 3).distanceTo(new SimpleLoc(4, 0)),
+				0.01);
 	}
-
 }
