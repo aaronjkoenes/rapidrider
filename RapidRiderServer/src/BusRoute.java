db075d8fe045d32d3b44ed6a8aeeb5673befc89c
anyninbign <anyninbign@1bd01c38-8c04-11dd-931e-57b92fbd641c>
2008-12-14 05:59:08 +0000

    Added JavaDoc to the server side code and generated the JavaDoc. Some of it is not really worth reading because it just echoes function and variable names.

--- RapidRiderServer/src/BusRoute.java
+++ RapidRiderServer/src/BusRoute.java
@@ -1,27 +1,45 @@
-
 import java.util.Vector;
 
+/**
+ * The BusRoute class models Rapid bus routes. Each bus route has a name and a
+ * list of stops.
+ */
 public class BusRoute {
 
 	private String routeName;
 	private Vector<BusStop> myStops;
 
-	// Creates a new bus route with a name.
+	/**
+	 * Creates a new bus route with a name.
+	 * 
+	 * @param name
+	 *            The name of the bus route.
+	 */
 	public BusRoute(String name) {
 		routeName = name;
-		// We can parameterize in the server!
 		myStops = new Vector<BusStop>();
 	}
 
+	/**
+	 * Returns the length of the route.
+	 * 
+	 * @return int
+	 */
 	public int routeLength() {
 		return myStops.size();
 	}
 
+	/**
+	 * Returns a String representation of the stops on this route.
+	 * 
+	 * @return String
+	 */
 	public String listStops() {
 		String temp = "";
-		if( myStops.size() > 0 ) {
+		if (myStops.size() > 0) {
 			for (int i = 0; i < myStops.size(); i++) {
-				temp += ((BusStop) getstop(i)).getName() + " : " + getstop(i).printLoc() + "\n";
+				temp += ((BusStop) getstop(i)).getName() + " : "
+						+ getstop(i).printLoc() + "\n";
 			}
 		} else {
 			temp = "No stops found";
@@ -29,35 +47,48 @@ public class BusRoute {
 		return temp;
 	}
 
+	/**
+	 * Adds a bus stop to this route.
+	 * 
+	 * @param stop
+	 *            The bus stop to add.
+	 */
 	public void addStop(BusStop stop) {
 		myStops.addElement(stop);
 	}
 
-	/* Note author:  Aaron
+	/**
+	 * Originally, if the user found the nearest stop more the once during a
+	 * single session, the stops would be added to the vector, without the old
+	 * ones being removed. This resulted in constant vector growth.
 	 * 
-	 * Originally, if the user found the nearest stop more the once during a single
-	 * session, the stops would be added to the vector, without the old ones being removed.
-	 * This resulted in constant vector growth.
-	 *
-	 * This method will remove all the previously known bus stops, and add the new ones,
-	 * saving space.
+	 * This method will remove all the previously known bus stops, and add the
+	 * new ones, saving space.
 	 * 
-	 * It is called immediately when the "Find Nearest" button is pressed (GPSMidlet.commandAction())
+	 * @author Aaron
 	 */
 	public void removeAllStops() {
 		myStops.removeAllElements();
 	}
 
+	/**
+	 * Gets the route name.
+	 * 
+	 * @return String
+	 */
 	public String getRouteName() {
 		return routeName;
 	}
 
 	/**
-	 * @param int
-	 * @return the BusStop at location (i) in the BusRoute
+	 * Gets the bus stop stored at the given index.
+	 * 
+	 * @param index
+	 *            The index of the BusStop on the route.
+	 * @return BusStop
 	 */
-	public BusStop getstop(int i) {
-		return (BusStop) myStops.elementAt(i);
+	public BusStop getstop(int index) {
+		return (BusStop) myStops.elementAt(index);
 	}
 
 }
--- RapidRiderServer/src/RapidRiderServlet.java
+++ RapidRiderServer/src/RapidRiderServlet.java
@@ -13,11 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
- * Servlet Class
- * 
- * TODO: More documentation should be added.
+ * The RapidRiderServlet class is a JBoss servlet.
  * 
  * @web.servlet name="RapidRider" display-name="Name for RapidRider"
  *              description="Description for RapidRider"
@@ -27,121 +24,159 @@ import javax.servlet.http.HttpServletResponse;
 public class RapidRiderServlet extends HttpServlet {
 	static final long serialVersionUID = 1;
 
-	Connection conn; // holds database connection
-	Statement stmt; // holds SQL statement
+	// Holds a database connection
+	Connection conn;
 
-	double targetLat = 0.0, targetLon = 0.0;
-	URL target;
+	// Holds an SQL statement
+	Statement stmt;
+
+	private double targetLat = 0.0, targetLon = 0.0;
+	private URL target;
 	private BusRoute route = new BusRoute("test Route");
 	private SimpleLoc currentLoc;
-	
-	// TODO: Add documentation?
+
+	/**
+	 * Handles an HTTP GET request. This method expects that the request
+	 * contains an address, a latitude, and a longitude.
+	 * 
+	 * @param req
+	 *            The request object.
+	 * @param resp
+	 *            The response object. The response is written to in a way
+	 *            similar to how standard output is written to.
+	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		System.out.println(req.getQueryString());
 
 		PrintWriter out = resp.getWriter();
 		out.println("<?xml version=\"1.0\"?>");
 		out.println("<rapidrider>");
-/*		if (req.getQueryString() != null) {		
-*/			String destinationAddress = req.getParameter("address");
-			double currentLat = Double.parseDouble(req.getParameter("lat"));
-			double currentLon = Double.parseDouble(req.getParameter("lon"));
-			currentLoc = new SimpleLoc(currentLat, currentLon);
-/*			System.out.println(req.getQueryString());
-*/			getDestinationCoords(destinationAddress);
-/*
-			out.println("<destLoc>");
-			out.println("<latitude>" + targetLat + "</latitude>");
-			out.println("<longitude>" + targetLon + "</longitude>");
-			out.println("</destLoc>");
-		} else {*/										
-			try {
-				Class.forName("org.postgresql.Driver");
-	
-				conn = DriverManager.getConnection("jdbc:postgresql:rapidrider",
-						"postgres", "bjarne");
-				stmt = conn.createStatement();
-				ResultSet res = stmt
-						.executeQuery("SELECT * FROM bus_stops");
-	
-				// TODO Question: Is it possible for res to be null?
-				if (res != null) {
-					while (res.next()) {
-						String stopName = res.getString(2);
-						double longitude = Double.parseDouble(res.getString(4));
-						double latitude = Double.parseDouble(res.getString(5));
-						
-						String revisedStopName = replaceEscapeCharacters(stopName);
-						BusStop temp = new BusStop(new SimpleLoc(latitude, longitude), revisedStopName);
-						route.addStop(temp);
-//						out.println("<busstop>");
-////						temp2 = route.getstop(route.routeLength());
-//						out.println("<stopName>" + temp.getName() + " " + temp.getLatitude() + " " + temp.getLongitude() + "</stopName>");
-//						out.println("</busstop>");
 
-/*						String latitude = res.getString(3);
-						String longitude = res.getString(4);
-						out.println("<busstop>");
-						out.println("<stopID>" + stopID + "</stopID>");
-						out.println("<stopName>" + revisedStopName + "</stopName>");
-						out.println("<latitude>" + latitude + "</latitude>");
-						out.println("<longitude>" + longitude + "</longitude>");
-						out.println("</busstop>");
+		String destinationAddress = req.getParameter("address");
+		double currentLat = Double.parseDouble(req.getParameter("lat"));
+		double currentLon = Double.parseDouble(req.getParameter("lon"));
+		currentLoc = new SimpleLoc(currentLat, currentLon);
 
-*/
-					}
-					out.println("<busstop>");
-					out.println("<stopName>" + findNearest(currentLoc).getName() + "</stopName>");
-					out.println("</busstop>");
-					out.println("<busstop>");
-					out.println("<stopName>" + findNearest(new SimpleLoc(targetLat, targetLon)).getName() + "</stopName>");
-					out.println("</busstop>");
+		getDestinationCoords(destinationAddress);
+
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			conn = DriverManager.getConnection("jdbc:postgresql:rapidrider",
+					"postgres", "bjarne");
+			stmt = conn.createStatement();
+			ResultSet res = stmt.executeQuery("SELECT * FROM bus_stops");
+
+			// TODO Question: Is it possible for res to be null?
+			if (res != null) {
+				while (res.next()) {
+					String stopName = res.getString(2);
+					String revisedStopName = replaceEscapeCharacters(stopName);
+					double longitude = Double.parseDouble(res.getString(4));
+					double latitude = Double.parseDouble(res.getString(5));
+
+					// I don't really understand what temporaryStop is or why it
+					// is added to the route. Also, what is route?
+					BusStop temporaryStop = new BusStop(new SimpleLoc(latitude,
+							longitude), revisedStopName);
+					route.addStop(temporaryStop);
+
+					// TODO Do we still want any of this?
+					// out.println("<busstop>");
+					// // temp2 = route.getstop(route.routeLength());
+					// out.println("<stopName>" + temp.getName() + " " +
+					// temp.getLatitude() + " " + temp.getLongitude() +
+					// "</stopName>");
+					// out.println("</busstop>");
+
+					// String latitude = res.getString(3); String longitude =
+					// res.getString(4); out.println("<busstop>");
+					// out.println("<stopID>" + stopID + "</stopID>");
+					// out.println("<stopName>" + revisedStopName +
+					// "</stopName>");
+					// out.println("<latitude>" + latitude + "</latitude>");
+					// out.println("<longitude>" + longitude + "</longitude>");
+					// out.println("</busstop>");
+
 				}
-				res.close();
-				stmt.close();
-				conn.close();
-	
-			} catch (Exception e) {
-				out.println("error in JDBC access: " + e.getClass() + " : "
-						+ e.getMessage());
-				e.printStackTrace();
+
+				BusStop nearestStop = findNearest(currentLoc);
+				out.println("<busstop>");
+				out.println("<stopName>" + nearestStop.getName()
+						+ "</stopName>");
+				out.println("</busstop>");
+				out.println("<busstop>");
+
+				// TODO what if TinyGeoCoder hasn't returned a value by the time
+				// we get to this part of the code?
+				out.println("<stopName>"
+						+ findNearest(new SimpleLoc(targetLat, targetLon))
+								.getName() + "</stopName>");
+				out.println("</busstop>");
 			}
-//		}
+			res.close();
+			stmt.close();
+			conn.close();
+
+		} catch (Exception e) {
+			out.println("Error in JDBC access: " + e.getClass() + " : "
+					+ e.getMessage());
+			e.printStackTrace();
+		}
 		out.println("</rapidrider>");
 	}
-		
+
+	/**
+	 * Returns the nearest BusStop to the given location out of all of the stops
+	 * in route.
+	 * 
+	 * @param l
+	 *            The location the find the nearest bus stop to.
+	 * @return BusStop
+	 */
 	public BusStop findNearest(SimpleLoc l) {
 		int nearestLocIndex = -1;
 		double distance = 0.0;
 		double shortest = -1.0;
-//		System.out.println(route.listStops());
-		int length = route.routeLength();
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < route.routeLength(); i++) {
 			double locationLat = route.getstop(i).getLatitude();
 			double locationLon = route.getstop(i).getLongitude();
 			SimpleLoc location = new SimpleLoc(locationLat, locationLon);
+
+			// TODO Isn't this the same as the location created in the above
+			// three lines?
+			route.getstop(i).getLoc();
+
 			distance = l.distanceTo(location);
-//			System.out.print("Distance found: " + distance);
+			// System.out.print("Distance found: " + distance);
 			if (shortest > distance || shortest < 0) {
-//				System.out.println(".  Shorter.  Setting to " + route.getstop(i).getName());
+				// System.out.println(". Shorter. Setting to " +
+				// route.getstop(i).getName());
 				shortest = distance;
 				nearestLocIndex = i;
 			} else {
-//				System.out.println();
+				// System.out.println();
 			}
 		}
 		return route.getstop(nearestLocIndex);
-		//nearestToDest.setText(route.getstop(nearestLocIndex).getName());
+		// nearestToDest.setText(route.getstop(nearestLocIndex).getName());
 	}
 
-	
-	/*
-	 * This portion of the code protects the XML. Ampersands (&) break XML
-	 * documents. This searches for ampersands and replaces them with the HTML
-	 * equivalent (&amp;)
+	/**
+	 * Uses TinyGeoCoder to set the destination coordinates from a description
+	 * of a place.
+	 * 
+	 * @param placeDescription
+	 *            The String to pass to TinyGeoCoder.
+	 * 
+	 * @throws IOException
 	 */
-	public void getDestinationCoords(String s) throws IOException {
-		String URLx = "http://tinygeocoder.com/create-api.php?q=" + replaceSpaces(s) ;
+	// TODO Shouldn't this method return its result?
+	public void getDestinationCoords(String placeDescription)
+			throws IOException {
+		String URLx = "http://tinygeocoder.com/create-api.php?q="
+				+ replaceSpaces(placeDescription);
 		System.out.println(URLx);
 		target = new URL(URLx);
 		InputStreamReader x = new InputStreamReader(target.openStream());
@@ -152,13 +187,26 @@ public class RapidRiderServlet extends HttpServlet {
 		parse(result);
 	}
 
-
-	public void parse(String s ) {
+	/**
+	 * Parses the latitude and longitude out of a string from TinyGeoCoder.
+	 * 
+	 * @param s
+	 *            The String from TinyGeoCoder
+	 */
+	public void parse(String s) {
 		String[] s2 = s.split(",");
 		targetLat = Double.parseDouble(s2[0]);
 		targetLon = Double.parseDouble(s2[1]);
-	}	
-	
+	}
+
+	/**
+	 * Replaces spaces in the given String with %20s.
+	 * 
+	 * @param s
+	 *            The String to alter.
+	 * 
+	 * @return String
+	 */
 	public String replaceSpaces(String s) {
 		String Result = "";
 		for (int i = 0; i < s.length(); i++) {
@@ -171,6 +219,14 @@ public class RapidRiderServlet extends HttpServlet {
 		return Result;
 	}
 
+	/**
+	 * Protects the XML by searching for ampersands and replacing them with the
+	 * HTML equivalent (&amp;)
+	 * 
+	 * @param s
+	 *            The String to modify.
+	 */
+	// TODO test.
 	private String replaceEscapeCharacters(String stopName) {
 		String revisedStopName = "";
 		// TODO: Should we use a character array instead of repeatedly adding to
@@ -184,8 +240,6 @@ public class RapidRiderServlet extends HttpServlet {
 				revisedStopName += c;
 			}
 		}
-		
 		return revisedStopName;
 	}
-
 }
